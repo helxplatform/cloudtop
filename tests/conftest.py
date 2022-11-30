e8fc6ff62972bea7eab9b6c2048b899d33a96987
Joshua Seals <jseals@renci.org>
2022-11-30 12:59:46 -0500

    Changing the pytest_adoption vars for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,4 +9,4 @@ def pytest_addoption(parser):
     parser.addoption("--image", action="store", default="default image")
     parser.addoption("--passwd", action="store", default="default image")
     parser.addoption("--port", action="store", default="8080")
-    parser.addoption("--user", action="store", default="default user")
+    parser.addoption("--user", action="store", default="root")
